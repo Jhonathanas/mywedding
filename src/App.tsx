343bb70411ac8b79c63aaca6db20b04f76c58b25
Jhonathanas <jonathanadisaputro99@students.amikom.ac.id>
2024-12-20 19:37:51 +0700

    header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import "./App.css";
 import ReactHowler from "react-howler";
+// import Timer from "./componets/Timer";
 function App() {
   const [isPlaying, setIsPlaying] = useState(false);
   return (
     <div className="bg-yellow-100">
       <div className="h-screen flex justify-center gap-20">
-        <div className="flex flex-col justify-center">
+        <div className="z-10 flex flex-col justify-center">
           <p className="text-5xl font-dancing">Jonathan Adi Saputro</p>
           <span className="text-9xl">&</span>
           <p className="text-5xl font-dancing">
@@ -19,23 +20,18 @@ function App() {
           <h1 className="font-spicy">This is Spicey Rice font</h1>
         </div>
         <img src="/duo.JPG" alt="" />
-
-        <div>
-          <h1>React Howler Audio Player</h1>
-          <ReactHowler src="/src/assets/song.mp3" playing={isPlaying} />
-          <button onClick={() => setIsPlaying(!isPlaying)}>
-            {isPlaying ? "Pause" : "Play"}
-          </button>
-        </div>
-        <audio controls autoPlay>
-          <source src="/song.mp3" type="audio/mp3" />
-          Browser Anda tidak mendukung audio HTML5.
-        </audio>
-        <audio controls autoPlay>
-          <source src="/assets/song.mp3" type="audio/mp3" />
-          Browser Anda tidak mendukung audio HTML5.
-        </audio>
       </div>
+      <div className="absolute z-20 h-10 w-10 rounded-full bottom-32 right-36 bg-red-500">
+        <ReactHowler src="/song.mp3" playing={isPlaying} />
+        <button onClick={() => setIsPlaying(!isPlaying)}>
+          {isPlaying ? "Pause" : "Play"}
+        </button>
+      </div>
+      <img className="absolute bottom-0  h-96" height={100}
+      src="/flower.png" alt="" />
+      <img className="absolute bottom-0 right-0  h-96" height={100}
+      src="/rightFlower.png" alt="" />
+      {/* <Timer/> */}
     </div>
   );
 }
