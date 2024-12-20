e20837834a17fe1ee29f5d950583fa88d819c4fa
Jhonathanas <jonathanadisaputro99@students.amikom.ac.id>
2024-12-20 19:53:13 +0700

    mobile

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,15 @@ import "./App.css";
 import ReactHowler from "react-howler";
 // import Timer from "./componets/Timer";
 function App() {
-  const [isPlaying, setIsPlaying] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(true);
   return (
     <div className="bg-yellow-100">
-      <div className="h-screen flex justify-center gap-20">
-        <div className="z-10 flex flex-col justify-center">
-          <p className="text-5xl font-dancing">Jonathan Adi Saputro</p>
-          <span className="text-9xl">&</span>
-          <p className="text-5xl font-dancing">
+      <div className="h-screen lg:flex justify-center gap-20">
+        <div className="lg:relative text-right absolute z-10 flex flex-col justify-center">
+          <p className="text-xl lg:text-5xl font-dancing">
+            Jonathan Adi Saputro <span className="text-xl">&</span>
+          </p>
+          <p className="text-xl lg:text-5xl font-dancing">
             Feronika Krismawati Devi Deria
           </p>
           <p className="font-poppins">
@@ -19,18 +20,29 @@ function App() {
           </p>
           <h1 className="font-spicy">This is Spicey Rice font</h1>
         </div>
-        <img src="/duo.JPG" alt="" />
+        <div className="h-screen flex">
+          <img className="m-auto rounded-full bg-red-400" src="/duo.JPG" alt="" />
+        </div>
       </div>
-      <div className="absolute z-20 h-10 w-10 rounded-full bottom-32 right-36 bg-red-500">
+      <div className="absolute z-20 h-10 w-10 rounded-full bottom-5 lg:bottom-32 right-5 lg:right-36 bg-red-500">
         <ReactHowler src="/song.mp3" playing={isPlaying} />
         <button onClick={() => setIsPlaying(!isPlaying)}>
           {isPlaying ? "Pause" : "Play"}
         </button>
       </div>
-      <img className="absolute bottom-0  h-96" height={100}
-      src="/flower.png" alt="" />
-      <img className="absolute bottom-0 right-0  h-96" height={100}
-      src="/rightFlower.png" alt="" />
+      <img
+        className="absolute bottom-0 lg:h-72 h-36"
+        height={70}
+        src="/flower.png"
+        alt=""
+      />
+      <img
+        className="absolute top-0 lg:h-72 h-36 rotate-90"
+        height={70}
+        src="/flower.png"
+        alt=""
+      />
+
       {/* <Timer/> */}
     </div>
   );
